cb0af8835c71fd002c334cb2e8493beb6510ad0b
Evgeny Toropov <etoropov@andrew.cmu.edu>
2013-11-08 22:32:39 -0500

    volleyball classes inside

--- CmuBusTracker/src/cmu18641/bustracker/ws/remote/RemoteQuery.java
+++ CmuBusTracker/src/cmu18641/bustracker/ws/remote/RemoteQuery.java
@@ -2,43 +2,51 @@ package cmu18641.bustracker.ws.remote;
 
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
 import cmu18641.bustracker.entities.*;
 import cmu18641.bustracker.exceptions.TrackerException;
+import android.content.Context;
 import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.text.format.Time;
 
 
 
-public class RemoteQuery implements Query {
+public class RemoteQuery { //implements Query {
 
-	@Override
-	public ArrayList<Bus> getBusesByStop(ArrayList<Stop> stops) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Time> getTimes (Context context, Stop stop, ArrayList<Bus> buses) throws TrackerException 
+	{
+		RequestQueue queue = Volley.newRequestQueue (context);
+		String url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBmSXUzVZBKQv9FJkTpZXn0dObKgEQOIFU&cx=014099860786446192319:t5mr0xnusiy&q=AndroidDev&alt=json&searchType=image";
+		
+		JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+	
+			@Override
+			public void onResponse(JSONObject response) {
+				Gson gson = new Gson();
+				ArrayList<Time> = gson.fromJson(response, ArrayList<Time>.class);
+				//txtDisplay.setText("Response => "+response.toString());
+				//findViewById(R.id.progressBar1).setVisibility(View.GONE);
+			}
+		}, new Response.ErrorListener() {
+	
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		queue.add(jsObjRequest);
 	}
-
-	@Override
-	public ArrayList<Stop> getStopsByCurrentLocation(Location here) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Stop> getStopByAddress(String street) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Stop> getStopByAddress(String street1, String street2) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Time> getTimes(Stop stop, ArrayList<Bus> buses) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
