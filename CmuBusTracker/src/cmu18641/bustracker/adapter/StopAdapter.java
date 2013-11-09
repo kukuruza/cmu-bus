288139bcbab067ec1121d17db621e35056bccc65
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-11-09 01:46:28 -0500

    adapter templates

---
+++ CmuBusTracker/src/cmu18641/bustracker/adapter/StopAdapter.java
@@ -0,0 +1,40 @@
+package cmu18641.bustracker.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/*
+ * StopAdapter.java
+ * 
+ * Maps stop arraylist to textviews on LocateStation activity
+ * and SearchStation activity
+ */
+
+public class StopAdapter extends BaseAdapter {
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+} // end StopAdapter
