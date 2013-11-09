d5b0a291ec8a30c5154e8866cdcc34be86f64855
Evgeny Toropov <etoropov@andrew.cmu.edu>
2013-11-08 22:40:10 -0500

    splits query into time and route (everything except time)

--- CmuBusTracker/src/cmu18641/bustracker/ws/remote/RemoteQuery.java
+++ CmuBusTracker/src/cmu18641/bustracker/ws/remote/RemoteQuery.java
@@ -1,39 +1,13 @@
 package cmu18641.bustracker.ws.remote;
 
 import java.util.ArrayList;
-
 import cmu18641.bustracker.entities.*;
 import cmu18641.bustracker.exceptions.TrackerException;
-import android.location.Location;
 import android.text.format.Time;
 
 
 
-public class RemoteQuery implements Query {
-
-	@Override
-	public ArrayList<Bus> getBusesByStop(ArrayList<Stop> stops) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Stop> getStopsByCurrentLocation(Location here) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Stop> getStopByAddress(String street) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Stop> getStopByAddress(String street1, String street2) throws TrackerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class RemoteQuery implements TimeQueryInterface {
 
 	@Override
 	public ArrayList<Time> getTimes(Stop stop, ArrayList<Bus> buses) throws TrackerException {
