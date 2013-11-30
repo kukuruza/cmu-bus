ead230d99e2d22e569f3b43c4e3737db59e7e9f1
Evgeny Toropov <etoropov@andrew.cmu.edu>
2013-11-30 12:35:41 -0500

    switched to PreferencesActivity from PreferencesFragment to satisfy requirement on API level 10

--- CmuBusTracker/src/cmu18641/bustracker/activities/SettingsActivity.java
+++ CmuBusTracker/src/cmu18641/bustracker/activities/SettingsActivity.java
@@ -1,18 +1,22 @@
 package cmu18641.bustracker.activities;
 
-import android.app.Activity;
+import cmu18641.bustracker.R;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
     public static final String KEY_REMOTE = "pref_remote";
 
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        // Implementing changing settings in PreferenceActivities is deprecated
+        //   but the recommended way - introduced in API-11 class PreferenceFragment -
+        //   is not allowed by project requirements, which say that API-10 must be supported.
+        //   Hence, we need to stick to deprecated method
+        addPreferencesFromResource(R.xml.preferences);
+        
     }
 }
